1c6afe481453e42de17b845172570c08f245d25c
Mazen <mazentamer2007@gmail.com>
2026-07-06 15:33:36 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,71 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include "utils.hpp"
 
 int main(){
 
-    std::cout << "Enter an email\n";
+    std::map<std::string,std::string> database;
+    int choice;
+    
     bool x = true;
+    bool isLogedin = false;
 
-    while(x){
-        std::string email;
-        std::cin >> email;
-        try{
-            check_email(email);
-            x = false; 
-        } catch(const std::exception& e){
-            std::cout << "email is invalid format, enter another email\n";
+    while(true){
+       
+        if(!isLogedin){
+            std::cout << "enter 1 to signUp and 2 to login\n";
+            std::cin >> choice;
+            if(choice == 1){
+                while(x){
+                    std::string email;
+                    std::string password;
+                    std::cout << "Enter valid email\n";
+                    std::cin >> email;
+                    std::cout << "Enter valid password\n";
+                    std::cin >> password;
+                    try{
+                        check_email(email);
+                        x = false;
+                        database[email] = password;
+                        isLogedin = true;
+                    } catch(const std::exception& e){
+                        std::cout << "email is invalid format, enter another email\n";
+                    }
+                }
+            }
+            else if(choice == 2){
+                std::string email;
+                std::string password;
+                std::cout << "Enter valid email\n";
+                std::cin >> email;
+                std::cout << "Enter valid password\n";
+                std::cin >> password;
+                for(std::pair p : database){
+                    if(p.first == email && p.second == password){
+                        std::cout << "Log in successful \n";
+                        isLogedin = true;
+                    }
+                }
+                if(!isLogedin)
+                    std::cout << "not found\n";
+            }
+        }
+        else if(isLogedin){
+            while(true){
+                std::cout << "type q to logout\n";
+                char x;
+                std::cin >> x;
+                if(x == 'q'){
+                    isLogedin = false;
+                    std::cout << "Log out successful\n";
+                    break;
+                }
+            }
+            
         }
     }
+        
     
     return 0;
 }
